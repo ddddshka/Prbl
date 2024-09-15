72bdd9c2f83e04af1f87b7a5d89e4fa1abef87c4
ddddshka <143650732+ddddshka@users.noreply.github.com>
2024-09-15 19:53:49 +0300

    vkeys.lua

--- hw.lua
+++ hw.lua
@@ -1,4 +1,6 @@
-local hw = {
-  "01C429AB-5B00-4D3D-95B2-2D7BB035F791"
+local vkeys = {
+  'ac2f6f80-56a0-4b6f-8840-c6b3db6d8c95',
+  'dfbd9fb2-3a10-4f6c-b9a8-a1a31ddf2437',
+  'admin'
 }
-return hw
+return vkeys
